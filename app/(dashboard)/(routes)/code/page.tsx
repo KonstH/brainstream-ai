969c9d1397863a7bac55161ad377eac3e82dde0b
KonstH <konstantinhristev@gmail.com>
2024-01-02 02:22:12 -0500

    Removes script, changes chat message margins logic

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -110,7 +110,7 @@ export default function CodePage() {
               <div
                 key={message.content as string}
                 className={cn(
-                  "p-8 w-full flex items-start gap-x-8 rounded-lg last:mb-8",
+                  "p-8 w-full flex items-start gap-x-8 rounded-lg first:mb-8",
                   message.role === "user" ? "bg-white border border-black/10" : "bg-slate-700/10"
                 )}
               >
